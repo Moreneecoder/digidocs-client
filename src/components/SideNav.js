d922d31ce10f1db9618bcb3971f7158f2f978714
Morenikeji Bello <caeserselassie@gmail.com>
2021-12-15 17:32:02 +0100

    Design Navbar to fit given design

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,14 +1,25 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import userAuth from '../helpers/userAuth';
 import '../styles/SideNav.css';
 
-const SideNav = () => (
-//   Sidebar
-  <div style={{ height: window.screen.availHeight }} className="SideNav border-end bg-white" id="sidebar-wrapper">
-    <h2 className="sidebar-heading pb-5 text-center">DigiDocs</h2>
-    <div style={{ width: '90%' }} className="list-group list-group-flush fw-bolder text-dark float-end">
-      <a className="list-group-item list-group-item-action list-group-item-light pl-1 py-2 text-white bg-success" href="#!">APPOINTMENTS</a>
-      <a className="list-group-item list-group-item-action list-group-item-light pl-1 py-2" href="#!">DOCTORS</a>
+const SideNav = () => {
+  const loggedIn = useSelector((state) => state.user);
+  let appointmentlink = '/login';
+
+  if (loggedIn || userAuth()) {
+    appointmentlink = '/';
+  }
+
+  return (
+    <div style={{ height: window.screen.height }} className="SideNav border-end bg-white" id="sidebar-wrapper">
+      <Link className="text-dark text-decoration-none text-center" to={appointmentlink}><h2 className="pt-3 pb-5">DigiDocs</h2></Link>
+      <div className="side-nav-links list-group list-group-flush fw-bolder float-end">
+        <Link className="text-decoration-none px-3 py-2 text-white main-bg-color" to={appointmentlink}>APPOINTMENTS</Link>
+        <Link className="text-decoration-none px-3 py-2 text-dark" to="/doctors">DOCTORS</Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SideNav;
--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -5,44 +5,34 @@ import '../styles/Navbar.css';
 
 const Navbar = () => {
   const loggedIn = useSelector((state) => state.user);
-  let appointmentlink = '/login';
-
-  if (loggedIn || userAuth()) {
-    appointmentlink = '/';
-  }
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-danger text-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark text-primary">
       <div className="container-fluid">
         <Link className="navbar-brand text-white fw-bolder" to="/">DigiDocs</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
         <div className="Navbar collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active text-white" aria-current="page" to={appointmentlink}>Appointments</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white" to="/fixtures">Fixtures</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white" to="/goalchart">Goal Chart</Link>
-            </li>
-          </ul>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0" />
 
-          <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ml-auto me-3 mb-2 mb-lg-0">
             {loggedIn || userAuth() ? (
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/logout">Logout</Link>
-              </li>
+              <>
+                <li className="nav-item">
+                  <span className="nav-link fw-bolder text-dark">UserName</span>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white main-bg-color px-3" to="/logout">Logout</Link>
+                </li>
+              </>
             ) : (
               <>
                 <li className="nav-item">
-                  <Link className="nav-link text-white" to="/login">Login</Link>
+                  <Link className="nav-link main-bg-color" to="/login">Login</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link text-white" to="/register">Register</Link>
+                  <Link className="nav-link main-bg-color" to="/register">Register</Link>
                 </li>
               </>
             )}
