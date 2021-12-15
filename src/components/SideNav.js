2f521d8c1404e8aa16654b2efd53aa1ed099a3fa
Morenikeji Bello <caeserselassie@gmail.com>
2021-12-15 16:53:16 +0100

    Structure Sidenav component to fit given design

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -2,15 +2,11 @@ import '../styles/SideNav.css';
 
 const SideNav = () => (
 //   Sidebar
-  <div className="SideNav border-end bg-white" id="sidebar-wrapper">
-    <h1 className="sidebar-heading border-bottom bg-light">DigiDocs</h1>
-    <div className="list-group list-group-flush">
-      <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard</a>
-      <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Shortcuts</a>
-      <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Overview</a>
-      <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Events</a>
-      <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Profile</a>
-      <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Status</a>
+  <div style={{ height: window.screen.availHeight }} className="SideNav border-end bg-white" id="sidebar-wrapper">
+    <h2 className="sidebar-heading pb-5 text-center">DigiDocs</h2>
+    <div style={{ width: '90%' }} className="list-group list-group-flush fw-bolder text-dark float-end">
+      <a className="list-group-item list-group-item-action list-group-item-light pl-1 py-2 text-white bg-success" href="#!">APPOINTMENTS</a>
+      <a className="list-group-item list-group-item-action list-group-item-light pl-1 py-2" href="#!">DOCTORS</a>
     </div>
   </div>
 );
--- src/containers/Appointments.js
+++ src/containers/Appointments.js
@@ -1,64 +1,66 @@
-// import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import userAuth from '../helpers/userAuth';
-import { useHttp } from '../hooks/http';
-import { loadAppointments } from '../actions';
-// import AppointmentModal from './AppoinmentModal';
+// // import { useNavigate } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import userAuth from '../helpers/userAuth';
+// import { useHttp } from '../hooks/http';
+// import { loadAppointments } from '../actions';
+// import '../styles/Appointments.css';
 
-const Appointments = () => {
-//   const navigate = useNavigate();
-  const loggedIn = useSelector((state) => state.user);
-  const appointments = useSelector((state) => state.appointments);
-  const userInfo = JSON.parse(localStorage.getItem('user'));
+// const Appointments = () => {
+// //   const navigate = useNavigate();
+//   const loggedIn = useSelector((state) => state.user);
+//   const appointments = useSelector((state) => state.appointments);
+//   const userInfo = JSON.parse(localStorage.getItem('user'));
 
-  if (loggedIn || userAuth()) {
-    useHttp(`http://digidocs-api.herokuapp.com/api/v1/${userInfo.role}/${userInfo.id}/appointments`,
-      loadAppointments,
-      []);
+//   if (loggedIn || userAuth()) {
+//     useHttp(`http://digidocs-api.herokuapp.com/api/v1/${userInfo.role}/${userInfo.id}/appointments`,
+//       loadAppointments,
+//       []);
 
-    if (appointments.length) {
-      console.log(appointments);
-      let idx = 0;
-      return (
-        <div className="Appointments">
-          <h1>Appointino</h1>
-          <Link className="btn btn-primary" to="/doctors">Book Apointment</Link>
-          {/* <AppointmentModal /> */}
+//     if (appointments.length) {
+//       console.log(appointments);
+//       let idx = 0;
+//       return (
+//         <div className="Appointments">
+//           <h1>Appointino</h1>
+//           <Link className="btn btn-primary" to="/doctors">Book Apointment</Link>
+//           {/* <AppointmentModal /> */}
 
-          {appointments.map((data) => {
-            idx += 1;
-            return (
-              <div key={idx}>
-                <span>
-                  <Link
-                //   index={rank}
-                    to={`/appointments/${data.appointment.id}`}
-                    state={{ id: data.appointment.id }}
-                  >
-                    {data.appointment.title}
-                  </Link>
-                </span>
-                {' '}
-&nbsp;
-                <span>{data.doctor.name}</span>
-                {' '}
-&nbsp;
-                <span>{data.appointment.time}</span>
-              </div>
-            );
-          })}
+//           {appointments.map((data) => {
+//             idx += 1;
+//             return (
+//               <div key={idx}>
+//                 <span>
+//                   <Link
+//                 //   index={rank}
+//                     to={`/appointments/${data.appointment.id}`}
+//                     state={{ id: data.appointment.id }}
+//                   >
+//                     {data.appointment.title}
+//                   </Link>
+//                 </span>
+//                 {' '}
+// &nbsp;
+//                 <span>{data.doctor.name}</span>
+//                 {' '}
+// &nbsp;
+//                 <span>{data.appointment.time}</span>
+//               </div>
+//             );
+//           })}
 
-        </div>
-      );
-    }
+//         </div>
+//       );
+//     }
 
-    return 'YOU HAVE NO APPOINTMENTS YET!';
-  }
+//     return 'YOU HAVE NO APPOINTMENTS YET!';
+//   }
 
-  //   navigate('/register');
-  window.location.href = '/login';
-  return null;
-};
+//   //   navigate('/register');
+//   window.location.href = '/login';
+//   return null;
+// };
+
+const Appointments = () => 'NO APPOINTMENTS';
 
 export default Appointments;
