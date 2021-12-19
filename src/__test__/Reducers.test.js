7ed3900dbb6852a6703dda6c6245c513c2c900e2
Morenikeji Bello <caeserselassie@gmail.com>
2021-12-19 21:22:20 +0100

    Fix linter errors

--- src/__test__/Reducers.test.js
+++ src/__test__/Reducers.test.js
@@ -1,21 +1,22 @@
 import userReducer from '../reducers/userReducer';
+
 const state = null;
 
 const userPayload = [
-    {
-      role: 'users',
-      data: {
-        created_at: "2021-12-19T13:16:53.469Z",
-        email: "ajide@example.com",
-        id: 23,
-        is_doctor: false,
-        name: "Ajide",
-        office_address: null,
-        phone: "+2347081520327",
-        updated_at: "2021-12-19T13:16:53.469Z",
-      },
+  {
+    role: 'users',
+    data: {
+      created_at: '2021-12-19T13:16:53.469Z',
+      email: 'ajide@example.com',
+      id: 23,
+      is_doctor: false,
+      name: 'Ajide',
+      office_address: null,
+      phone: '+2347081520327',
+      updated_at: '2021-12-19T13:16:53.469Z',
     },
-  ];
+  },
+];
 
 const action = {
   type: 'AUTH_SUCCESS',
