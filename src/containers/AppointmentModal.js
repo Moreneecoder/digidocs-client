ee633b356b1e1877e0096e318ff342bef748e08d
Morenikeji Bello <caeserselassie@gmail.com>
2021-12-19 23:40:18 +0100

    Make book button white in AppointmentModal component

--- src/containers/AppointmentModal.js
+++ src/containers/AppointmentModal.js
@@ -92,7 +92,7 @@ const AppointmentModal = (props) => {
               </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit" className="btn px-3 main-bg-color">Book</button>
+                <button type="submit" className="btn px-3 main-bg-color text-white">Book</button>
               </div>
             </div>
           </form>
